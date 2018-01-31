2dbcdacf9f7724441a1e83cdc513b98e6ab560a7
jamalsiam <jamalsiam1995@gmail.com>
2018-01-31 12:39:53 +0200

    work with array

--- php/array.php
+++ php/array.php
@@ -26,6 +26,24 @@ echo "itemsarray[2][1]:". $itemsarray[2][1];
 echo "<br/>";
 echo "itemsarrayIn:".  print_r($itemsarray[2]);
  ?>
+ <?php
+$numbers = array(1,10,5,20,15,60,8,27);
+echo print_r($numbers );
+ ?>
+</br>
+ Max number: <?php    echo max($numbers );   ?>
+ </br>
+ Min number: <?php    echo min($numbers );   ?>
+ </br>
+ Count number: <?php    echo count($numbers );   ?>
+  <br/>
+  Arry order sort <?php sort($numbers ); echo print_r($numbers );   ?>
+    <br/>
+  Arry rever sort <?php rsort($numbers ); echo print_r($numbers );   ?>
 
+  <br/>
+  Is 10 in array <?php echo in_array(10, $numbers );     ?>
+   <br/>
+  Is 12 in array <?php echo in_array(12, $numbers );     ?>
 </body>
 </html>
