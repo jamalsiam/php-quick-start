544b1be817b3d544d0092ee3c317cdcf3dd5b295
jamalsiam <jamalsiam1995@gmail.com>
2018-01-31 11:29:36 +0200

    work with variable

---
+++ php/variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP</title>
+</head>
+<body>
+<?php
+$txt="hello php";
+$num1=10;
+$num2=15;
+echo $txt;
+echo '<h2>'.$num1.'</h2>';
+echo '<h2>'.($num1+$num2).'</h2>';
+$num3=$num2;
+$num3=2;
+echo '<h2>'.$num2.'</h2>';
+echo '<h2>'.$num3.'</h2>';
+
+?>
+</body>
+</html>
